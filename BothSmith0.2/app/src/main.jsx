dad997977be7626b3324445eebba43771ef092c5
Zephyrus2822 <rudranilchowdhury83@gmail.com>
2024-10-15 21:11:00 +0530

    fuck it we ball

--- BothSmith0.2/app/src/main.jsx
+++ BothSmith0.2/app/src/main.jsx
@@ -1,14 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Botbuilder from './components/BotBuilder'
-// import App from './App.jsx'
-import './index.css'
-import LandingPage from './components/LandingPage'
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import BotBuilder from './components/BotBuilder';
+import Home from './components/Home';  // Import Home component
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <Botbuilder/> */}
-    <LandingPage/>
-  </StrictMode>,
-)
+    
+    <BrowserRouter>
+  
+      <Routes>
+        {/* Define routes for components */}
+        <Route path="/" element={<LandingPage />} /> {/* Landing Page as the default route */}
+        <Route path="/home" element={<Home />} />     {/* Route for Home page */}
+        <Route path="/botbuilder" element={<BotBuilder />} /> {/* Route for Bot Builder */}
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
